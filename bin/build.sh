ad1ad64fcbda7ce28b48801d9ff9d85a6dd5bcda
Kevin Wickham <kwickham@iastate.edu>
2021-11-16 09:10:39 -0600

    added rsync for prod

--- bin/build.sh
+++ bin/build.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
 php bin/satis build satis.json build
+
+if [ -d "/var/www/myfiles/html.engineering.iastate.edu/www/composer/" ]; then
+    rsync -rtq --delete /usr/people/engrweb/satis-composer/build/ /var/www/myfiles/html.engineering.iastate.edu/www/composer/
+fi
